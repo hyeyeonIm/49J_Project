1e6f0a3ea9dc5c9d6fcade63a7e660d946c9cdac
hyeyeonIm <enhyeonii@gmial.com>
2022-12-04 17:56:52 -0800

    ranking

--- Ranking.java
+++ Ranking.java
@@ -1,43 +1,46 @@
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import java.awt.*;
-
 public class Ranking extends JFrame{
+	private JFrame jf;
+	private JTable jtable;
+	private JScrollPane jsp;
+
+    static String contents[][] = {
+        // {"Jake", "AI", "Male", "8"},
+        // {"Haley", "ME", "Female", "10"},
+        // {"Minse", "CS", "Male", "6"},
+        // {"mike", "test", "ai", "0"}
+    };
+
         public Ranking() {
-            super("RANKING");
             // LOAD players from text file in Player Class with Linked List Data Structure
-
             // SORT player instances and extract TOP 3 players
+            jf = new JFrame("RANKING");
+            jf.setLocationRelativeTo(null);
+            jf.setSize(500, 500);
+            Color color1 = new Color(198,218,214);
+            Color color2 = new Color(109,146,155);
 
-                // Save top 3 players' name, major, gender, SCORE in local variables
+            String header[] = {"name", "major", "gender", "score"};
+            jtable = new JTable(contents, header);
+		    jsp = new JScrollPane(jtable);
 
-            JPanel jPanel = new JPanel();
             JButton btn3 = new JButton("AGAIN");
             JButton btn4 = new JButton("END");
     
-            setSize(500, 500); // size
-            Color color1 = new Color(198,218,214);
-            Color color2 = new Color(109,146,155);
-    
-            jPanel.setBackground(color1);
-    
-            jPanel.add(btn3);
-            jPanel.add(btn4);
-            jPanel.add(btn3, BorderLayout.WEST) ;
-            jPanel.add(btn3, BorderLayout.EAST) ;
-    
+            jf.setBackground(color1);
             btn3.setBackground(color2);
             btn4.setBackground(color2);
-            add(jPanel);
-    
-            // MAKE LABELS to show top 3 players
 
+            jf.add(jsp, BorderLayout.NORTH);
+            jf.add(btn3, BorderLayout.WEST);
+            jf.add(btn4, BorderLayout.EAST);
+            jf.setVisible(true);
 
-
-            setVisible(true);
-            
             btn3.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
