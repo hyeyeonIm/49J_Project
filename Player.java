61331273caf18497ad30574fa72f6ac5b364fa1a
och9854 <och9854@naver.com>
2022-12-04 18:44:36 -0800

    sorting with linkedList

--- Player.java
+++ Player.java
@@ -1,31 +1,56 @@
-public class Player {
-    private String[] info={"","",""};
-    private int score=0;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
-    // Other classes can get info and score
-    public String[] getPlayer(){
-        return this.info;
-    }
-    public int getScore(){
-        return score;
-    }
+public class Player implements Comparable<Player> {
+    String name;
+    String major;
+    String gender;
+    int score;
 
-    // Other class can change in info and score
-    public void setPlayer(String name_info, String major_info, String gender_info){
-        info[0]=name_info;
-        info[1]=major_info;
-        info[2]=gender_info;
-    }
-    public void setScore(int score){
-        this.score=score;
+    Player (String name, String major, String gender, int score){
+        this.name = name;
+        this.major = major;
+        this.gender = gender;
+        this.score = score;
     }
 
-                        // Store in Player class
-                        // player1.setPlayer(name_info,major_info,gender_info);
-                        // player1.setScore(1);
-        
-                        // System.out.println(player1.getPlayer()[0]);
-                        // System.out.println(player1.getPlayer()[1]);
-                        // System.out.println(player1.getPlayer()[2]);
-                        // System.out.println(player1.getScore());
+    // Override the compareTo() method
+    @Override public int compareTo(Player p)
+    {
+        if (score > p.score) {
+            return 1;
+        }
+        else if (score == p.score) {
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
+
+
+    // private String[] info={"","",""};
+    // private int score=0;
+
+    // // Other classes can get info and score
+    // public String[] getPlayer(){
+    //     return this.info;
+    // }
+    // public int getScore(){
+    //     return score;
+    // }
+
+    // // Other class can change in info and score
+    // public void setPlayer(String name_info, String major_info, String gender_info){
+    //     info[0]=name_info;
+    //     info[1]=major_info;
+    //     info[2]=gender_info;
+    // }
+    // public void setScore(int score){
+    //     this.score=score;
+    // }
+
+    
+
 }
--- Ranking.java
+++ Ranking.java
@@ -4,19 +4,95 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import java.awt.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Ranking extends JFrame{
 	private JFrame jf;
 	private JTable jtable;
 	private JScrollPane jsp;
 
-    static String contents[][] = {
-        // {"Jake", "AI", "Male", "8"},
-        // {"Haley", "ME", "Female", "10"},
-        // {"Minse", "CS", "Male", "6"},
-        // {"mike", "test", "ai", "0"}
-    };
+    static String contents[][] = new String[3][4];
+    // { 
+    //     // {"Jake", "AI", "Male", "8"},
+    //     // {"Haley", "ME", "Female", "10"},
+    //     // {"Minse", "CS", "Male", "6"},
+    //     // {"mike", "test", "ai", "0"}
+    // };
 
         public Ranking() {
+            //test
+            BufferedReader reader1;
+            BufferedReader reader2;
+
+            try {
+                // Count number of lines(Players) in the file
+                reader1 = new BufferedReader(new FileReader("player.txt"));
+                int lines = 0; // variable which stores number of players
+
+                while (reader1.readLine() != null) {
+                    lines++;
+                }
+                reader1.close();
+
+                
+                // Create Instances using "lines"
+                reader2 = new BufferedReader(new FileReader("player.txt"));
+                // Player [] players = new Player [lines]; 
+                
+
+                LinkedList<Player> playerList = new LinkedList<Player>(); // Make a linkedlist to save player instances
+                String line2 = reader2.readLine(); // read first line
+                for (int i = 0; i < lines; i ++) {
+                    // System.out.println(line2);
+                    String[] Player_info = line2.split(", ");
+                    playerList.add(new Player(Player_info[0], Player_info[1], Player_info[2], Integer.parseInt(Player_info[3])));
+                    line2 = reader2.readLine(); // go to next line
+                    // Player player = new Player(Player_info[0], Player_info[1], Player_info[2], Integer.parseInt(Player_info[3]));
+                    // player.setPlayer();
+                    // player.setScore();
+                    // players[i] = player;
+                }
+
+                reader2.close();
+
+                // Sort and Print player
+                Collections.sort(playerList, Collections.reverseOrder());
+                int cnt = 0;
+                int topN = 2;
+                for (Player p : playerList) {
+                    // System.out.println(p.name + " " + p.major + " " + p.gender + " " + p.score);
+                    contents[cnt][0] = p.name;
+                    contents[cnt][1] = p.major;
+                    contents[cnt][2] = p.gender;
+                    contents[cnt][3] = String.valueOf(p.score);
+            
+                    if (cnt >= topN) {
+                        break;
+                    }
+                    else{
+                        cnt++;
+                    }
+                }
+                System.out.println();
+
+                // pass to FRAME
+                // Player [] topThreePlayers = new Player [3];
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            
+        
+
+            // end test
             // LOAD players from text file in Player Class with Linked List Data Structure
             // SORT player instances and extract TOP 3 players
             jf = new JFrame("RANKING");
@@ -45,7 +121,10 @@ public class Ranking extends JFrame{
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     new input(); // Go MainFrame
+                    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                     setVisible(false); // invisible
+                    dispose();
+
                 }
             });
     
@@ -59,5 +138,6 @@ public class Ranking extends JFrame{
 
     public static void main(String[] args) {
         new Ranking();
+        
     }
 }
